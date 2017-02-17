6ddaeea9171091f0d3b60a2aa055ade155d2a0f3
Alexey <symblight@gmail.com>
2017-02-17 14:31:23 +0300

    Work with panel info

--- client/componets/Account/AccountPanel.jsx
+++ client/componets/Account/AccountPanel.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
-import {Row,Col,Grid, Panel} from 'react-bootstrap';
 import * as api from '../../../etc/data.js';
 import './Info.less';
+
+class Avatar extends React.Component{
+    render(){
+        const {avatar} = this.props;
+
+        return <div className="avatar_l">
+            <div className="avatar_i">
+                <img src={avatar} className="avatar"/>
+            </div>
+        </div>
+    }
+}
+
+class MenuInfo extends React.Component{
+    render(){
+        const {userName}= this.props;
+        const {firstName} = this.props;
+
+        return<div className="menu_info">
+            <div className="btn_name">
+                <div className="login_i">
+                    <h1 className="Login">{userName}</h1>
+                </div>
+                <div className="btn_layout">
+                    <button>Edit account</button>
+                </div>
+            </div>
+            <div className="post_i">
+                <div className="item_i">count post: 4</div>
+                <div className="item_i">followers: 2</div>
+                <div className="item_i">following: 2</div>
+            </div>
+            <div className="name_i">
+                <div className="item_nameInfo"><h4>{firstName}</h4></div>
+            </div>
+        </div>
+    }
+}
 
 export default class InfoPanel extends React.Component{
     constructor(props){
@@ -12,13 +49,8 @@ export default class InfoPanel extends React.Component{
     }
     render(){
         return<header className="container_i">
-                <div className="avatar_l"><img src={api.Account.avatar} className="avatar"/></div>
-                <div className="menu_info">
-                    <div className="btn_name">
-                        <div><h2>{api.Account.username}</h2></div>
-                        <div><button>Edit account</button></div>
-                    </div>
-                </div>
+                <Avatar avatar={api.Account.avatar}/>
+                <MenuInfo firstName={api.Account.first_name} userName={api.Account.username}/>
         </header>
     }
 }
